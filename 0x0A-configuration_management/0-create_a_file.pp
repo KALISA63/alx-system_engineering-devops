fa448be6f6d65cab9010ee199adab35e6e0d42cb
KALISA63 <kaljama63@gmail.com>
2023-01-13 20:47:40 +0200

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creates a file in /tmp
+#create a file in /tmp
 
-file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+file {'/tmp/school':
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
